7fa7fac123c3de1487ae04b93b30afa402f5585a
tanukiraccoon <donut.158@hotmail.com>
2022-05-26 16:06:21 +0700

    remove os

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-import psutil
+# import os
+# import psutil
 from flask import Flask, render_template, request, jsonify
 from lu import th2lu
 
@@ -15,11 +15,10 @@ def index():
 def process():
     if text := str(request.form['text']):
         result = th2lu(text)
-        print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+        # print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
         return jsonify({'result': result})
     return jsonify({'error': 'Missing data!'})
 
 
 if __name__ == '__main__':
-    app.debug = False
-    app.run()
+    app.run(debug=False, host='0.0.0.0')
