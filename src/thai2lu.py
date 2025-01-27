2e86566816279eb55601f1e558f01347f1b10dc9
tanukiraccoon <donut.158@hotmail.com>
2025-01-27 10:45:42 +0700

    update code related to new ipa_transcriber

--- src/thai2lu.py
+++ src/thai2lu.py
@@ -1,70 +1,65 @@
 # -*- coding: utf-8 -*-
 from tltk import th2ipa
-from ..thai-ipa-transcriber.src import ipa_transcriber
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'thai-ipa-transcriber', 'src')))
+
+from ipa_transcriber import ThaiIPA
+
 import copy
 
-def extract_ipa_list(txt: str) -> list[str]:
+def process_ipa_text(txt: str) -> list[str]:
     try:
-        ipas = th2ipa(txt).replace(' <s/>', '').replace('.', ' ')
+        ipas = th2ipa(txt)
+        
+        replace_str = {
+        ' <s/>': ' ',
+        '.': ' ',
+        '-': '', # Handle คาปูชิโน่หวานน้อย kʰaː1 puː1 cʰi4 noː-3 waːn1 nᴐːj4 <s/>
+        }
+            
+        for key ,value in replace_str.items():     
+            ipas = ipas.replace(key, value)
+        ipas = ipas.strip()    
         return [ipa for ipa in ipas.split(' ') if ipa.strip()]
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        print(txt)
+        print(f"Input text: {txt}")
         return []
-
-def gen_lu_first(obj: IPA) -> str:
-
+    
+def generate_lu_first_syllable(obj: ThaiIPA) -> str:
     o = copy.deepcopy(obj)
     
+    #คำที่มี ร หรือ ล จะถูกแทนด้วย ซ เช่น ลอง -> ซอง และ รัก -> ซัก
     if o.initial_en in ['l','r']:
         o.initial_en = 's'
     else:
         o.initial_en = 'l'
-
+   
     o.cluster_en = ''
-    o.process()
 
     return o.spell()
 
-def gen_lu_second(obj: IPA) -> str:
-
+def generate_lu_last_syllable(obj: ThaiIPA) -> str:
     o = copy.deepcopy(obj)
-    
-    if o.long_vow:
-        o.vowel_th = 'อี-' if o.vowel_en == 'uː' else 'อู-'
-    elif o.vowel_en == 'u':
-        o.vowel_th = 'อิ-'
+    # คำที่มีสระอุหรืออู ให้เติม 'สระอี' สำหรับเสียงยาว และ 'สระอิ' สำหรับเสียงสั้น
+    # คำที่ไม่มีสระอุหรืออู ให้เติม 'สระอู' สำหรับเสียงยาว และ 'สระอุ' สำหรับเสียงสั้น
+    if o.vowel_en in ['u','uː']:
+        o.vowel_en = 'iː' if o.is_long else 'i'
     else:
-        o.vowel_th = 'อุ-'
+        o.vowel_en = 'uː' if o.is_long else 'u'
+
     return o.spell() 
 
-def gen_lu_all(obj :IPA) -> str:
-    return gen_lu_first(obj) + gen_lu_second(obj)
+def generate_lu_all_syllable(obj :ThaiIPA) -> str:
+    return generate_lu_first_syllable(obj) + generate_lu_last_syllable (obj)
 
-def thai2lu(txt: str) -> list[str]:
+def convert_to_lu(txt: str) -> list[str]:
     results  = []
-    for ipa in extract_ipa_list(txt) :
-        obj  = IPA(ipa)
-        result  = gen_lu_all(obj)
+    for ipa in process_ipa_text(txt) :
+        obj  = ThaiIPA(ipa)
+        result  = generate_lu_all_syllable(obj)
         results.append(result)
 
     return results
-
-def gen_lu2thai(objs: list):
-    fs = objs[0]
-    ls = objs[-1]
-    ls.vowel_th = fs.vowel_th
-    return ls.spell()
-
-def lu2thai(txt:str)-> list[str]:
-    txts = txt.split(' ')
-    results =[]
-    for i in txts:
-        objs = []
-        for ipa in extract_ipa_list(i):
-            obj = IPA(ipa)
-            objs.append(obj)
-        result = gen_lu2thai(objs)
-        results.append(result)
-    return results
-    
